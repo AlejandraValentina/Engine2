c47df3feb42b33d41d5e02630a0d0120112ba8c4
AlejandraValentina <48266009+AlejandraValentina@users.noreply.github.com>
2025-11-24 10:34:01 -0300

    Simulate four-stroke valve-driven pulses

--- main.py
+++ main.py
@@ -322,19 +322,24 @@ class MainWindow(QMainWindow):
             return
 
         steps_per_frame = self.speed_slider.value()
-        p_cyl = 1_000_000.0
+        rpm = 12_000.0
+        p_high = 15.0 * 100_000.0
+        p_low = 1.0 * 100_000.0
         t_cyl = 1200.0
         max_area = 0.0007
 
         for _ in range(steps_per_frame):
-            t = self.solver.time
-            if t < 0.005:
-                current_area = max_area * (t / 0.005)
+            crank_angle = (self.solver.time * rpm * 360.0 / 60.0) % 720.0
+            if 140.0 < crank_angle < 360.0:
+                lift_factor = np.sin(np.pi * (crank_angle - 140.0) / (360.0 - 140.0))
+                current_area = max_area * lift_factor
+                current_p_cyl = p_high
             else:
-                current_area = max_area
+                current_area = 0.0
+                current_p_cyl = p_low
 
             dt = self.solver.get_time_step()
-            self.solver.step_valve_boundary(dt, p_cyl, t_cyl, current_area)
+            self.solver.step_valve_boundary(dt, current_p_cyl, t_cyl, current_area)
             self.solver.step(dt)
 
         x_axis = np.linspace(0, self.solver.L, self.solver.N)
